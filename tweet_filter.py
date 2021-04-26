27881ae9f249164825b03d0bb9fb0c621876d12b
jphuber2 <unconfigured@null.spigotmc.org>
2021-04-26 18:19:20 -0400

    uh, changed the filter script

--- tweet_filter.py
+++ tweet_filter.py
@@ -1,7 +1,9 @@
 import pandas as pd
 
-keywords = ["tesla", "TSLA", "Tesla"]
+keywords = ["tesla", "TSLA", "Tesla", "stock", "market", "stock market", "$TSLA"]
 searched_keywords = '|'.join(keywords)
 df = pd.read_csv("elonmusk.csv", sep=",")
 df = df[df["tweet"].str.contains(searched_keywords)]
+df = df.sort_values(['date','likes_count'],ascending=True)
+df = df.drop_duplicates(subset='date', keep="last")
 df.to_csv("elon_filtered.csv", sep=",", index=False)
