4f04347881f60cd78956047b32dd5846fc3b035a
adixit3 <adixit3@ncsu.edu>
2021-04-26 18:39:42 -0400

    Updated filter to sort tweets by date and created new CSV

--- tweet_filter.py
+++ tweet_filter.py
@@ -2,8 +2,9 @@ import pandas as pd
 
 keywords = ["tesla", "TSLA", "Tesla", "stock", "market", "stock market", "$TSLA"]
 searched_keywords = '|'.join(keywords)
-df = pd.read_csv("elonmusk.csv", sep=",")
+df = pd.read_csv("data_with_total_engagement.csv", sep=",")
 df = df[df["tweet"].str.contains(searched_keywords)]
-df = df.sort_values(['date','likes_count'],ascending=True)
+df['date'] = pd.to_datetime(df['date'])
+df = df.sort_values(['date', 'total_engagement'], ascending=True)
 df = df.drop_duplicates(subset='date', keep="last")
-df.to_csv("elon_filtered.csv", sep=",", index=False)
+df.to_csv("total_engagement_filtered.csv", sep=",", index=False)
