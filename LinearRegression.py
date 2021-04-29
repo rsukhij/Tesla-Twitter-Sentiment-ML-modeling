807a24f4aa4e4f1d3d978562e3164ebecad47d12
rsukhij <rsukhij@ncsu.edu>
2021-04-29 18:28:01 -0400

    linear reg scatter plot update

--- LinearRegression.py
+++ LinearRegression.py
@@ -4,19 +4,21 @@ import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
-
+import datetime as dt
 
 # Main function
 def main():
     # Get dataframe from cleaned CSV of data
     df = pd.read_csv('total_engagement_filtered.csv')
-
+    df['Date'] = pd.to_datetime(df.date)
+    df['date'] = pd.to_datetime(df.date)
+    df['date']= df['date'].map(dt.datetime.toordinal)
     # X data
-    sentiment_column = df.loc[:, 'compound']
+    sentiment_column = df.loc[:, 'date']
     sentiment = sentiment_column.values
 
     # Y data
-    close_price_column = df.loc[:, 'Percent change 1 week']
+    close_price_column = df.loc[:, 'compound']
     close_price = close_price_column.values
 
     # Convert the data into numpy arrays
@@ -25,10 +27,9 @@ def main():
 
     # Split data intro training and test arrays 80/20 split
     X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
-
+    plt.scatter(df['date'],df['compound'])
     # Create the Linear Regression Model and fit it on the training data
     model = LinearRegression().fit(X_train, y_train)
-
     # Test it on the training data and get R^2 value to see goodness of fit
     r_sq = model.score(X_test, y_test)
     print("R squared value: ", r_sq, "\n")
@@ -45,8 +46,8 @@ def main():
     y_val = model.intercept_ + model.coef_ * x_val
     plt.plot(x_val, y_val, '--')
     plt.title("Linear Regression Model")
-    plt.xlabel("Sentiment")
-    plt.ylabel("Closing price")
+    plt.xlabel("Ordinal Date since 2015")
+    plt.ylabel("Sentiment")
     plt.show()
 
 
